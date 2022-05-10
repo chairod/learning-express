75796885d968c6bba3cf2be7537039ea23a2ba1b
chairod <chairod.p@live.com>
2022-05-10 23:35:19 +0700

    Plain Server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const path = require('path');
-const compression = require('compression');
-const bodyParser = require('body-parser');
-const appRoutes = require('./public/app-routes');
+//const path = require('path');
+//const compression = require('compression');
+//const bodyParser = require('body-parser');
+//const appRoutes = require('./public/app-routes');
 
 
 const app = express();
@@ -11,25 +11,25 @@ const serverPort = process.env.PORT || 5000;
 // กำหนด Static root path
 // ให้กับ Server
 // เรียกใช้งานผ่าน Browser domain:port/filename.ext ได้เลย โดยไม่ต้อง Refer พาร์ทที่อยู่ของไฟล์
-app.use(express.static(path.join(__dirname, 'public/static/pages')));
+//app.use(express.static(path.join(__dirname, 'public/static/pages')));
 //app.use(express.static(path.join(__dirname, 'static')));
 
 // รองรับ Post application/json
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 // รอบรับ Post x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.urlencoded({extended: true}));
 
 // Foundation web security protection
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
 
-    // Optimize http header
-    res.append('X-Content-Type-Options', 'nosniff');
-    res.append('X-Frame-Options', 'DENY');
-    res.append('X-XSS-Protection', '1; mode=block');
+//     // Optimize http header
+//     res.append('X-Content-Type-Options', 'nosniff');
+//     res.append('X-Frame-Options', 'DENY');
+//     res.append('X-XSS-Protection', '1; mode=block');
 
-    // Process to next
-    next();
-});
+//     // Process to next
+//     next();
+// });
 
 // Http Compression
 //app.use(compression());
