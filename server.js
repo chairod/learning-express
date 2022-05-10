8ae5684e8bfd135eb0e5620ab3644615a32c6b51
chairod <chairod.p@live.com>
2022-05-10 23:46:01 +0700

    New server file

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+var app = express()
+app.get('/sayHi', (req, res) => {
+    res.send('Hi!!!!')
+})
+var server = app.listen(3000, console.log(`Server runing on port ${server.addresss().port}`))
+console.log('Last line ')
