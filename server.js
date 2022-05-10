f3052e9f3fc5f2a24df30663eaca653f48a425bf
chairod <chairod.p@live.com>
2022-05-10 23:49:01 +0700

    Fixed Server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express');
 
 var app = express()
 app.get('/sayHi', (req, res) => {
-    res.send('Hi!!!!')
-})
-var server = app.listen(3000, console.log(`Server runing on port ${server.addresss().port}`))
-console.log('Last line ')
+    res.send('Hi!!!!');
+});
+
+var server = app.listen(3000, () => {
+    console.log(`Server runing on port ${server.addresss().port}`);
+});
+console.log('Last line ');
