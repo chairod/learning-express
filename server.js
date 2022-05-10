d60bcf583c965fd0315908728b36b9ec0c909e2a
chairod <chairod.p@live.com>
2022-05-11 00:23:11 +0700

    Export server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+var app = express()
+app.get('/sayHi', (req, res) => {
+    res.send('Hi!!!!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(3000, () => {
+    console.log(`Server runing on port 3000`);
 });
+
+module.exports = app;
